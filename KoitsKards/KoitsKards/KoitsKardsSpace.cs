bcd530a6e97352192e0fdf2be0b854878ad1a221
kurt-urban <80078706+Kurt-Urban@users.noreply.github.com>
2022-05-29 21:42:39 -0400

    feat: template setup

---
+++ KoitsKards/KoitsKards/KoitsKardsSpace.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using BepInEx;
+
+using UnboundLib;
+using UnboundLib.Cards;
+using KoitsKardsSpace.Cards;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+
+namespace KoitsKardsSpace
+{
+    // These are the mods required for our mod to work
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+    // Declares our mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our mod is associated with
+    [BepInProcess("Rounds.exe")]
+    public class KoitsKards : BaseUnityPlugin
+    {
+        private const string ModId = "Koits.Kards.Id";
+        private const string ModName = "KoitsKards";
+        public const string Version = "1.0.0"; // What version are we on (major.minor.patch)?
+
+        public const string ModInitials = "KK";
+
+        public static KoitsKards instance { get; private set; }
+
+        void Awake()
+        {
+            // Use this to call any harmony patch files your mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+        void Start()
+        {
+            instance = this;
+            CustomCard.BuildCard<EvasiveManeuvers>();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Koits-Kards
